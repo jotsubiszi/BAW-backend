9439ab5107519b7d6ab14db2c39ef9d5b941472b
jotsubiszi <szynszylxz@gmail.com>
2026-03-16 18:46:03 +0100

    Well that was annoying

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,72 @@
 use axum::{Router, extract::Path, http::StatusCode, response::Json, routing::get};
-use pokemon_tcg_sdk::{
-    card::{GetCardRequest, SearchCardsRequest},
-    client::Client,
-};
 use serde_json::{Value, json};
+use tcgdex_api::{CardBrief, Query, Tcgdex};
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/pokeapi/{poke}", get(print_pokemon));
-    // .route("/tcgapi/{api}/card/{card}", get(tcg_api_handler));
+    let app = Router::new()
+        .route("/pokeapi/{poke}", get(rustemon_api_handler))
+        .route("/tcgapi/{poke}", get(tcg_api_handler));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn print_pokemon(Path(poke_name): Path<String>) -> Result<Json<Value>, (StatusCode, String)> {
+#[derive(serde::Serialize)]
+struct CardBriefResponse {
+    id: String,
+    local_id: String,
+    name: String,
+    image: String,
+}
+
+impl From<CardBrief> for CardBriefResponse {
+    fn from(c: CardBrief) -> Self {
+        Self {
+            id: c.id,
+            local_id: c.local_id,
+            name: c.name,
+            image: c.image,
+        }
+    }
+}
+
+async fn rustemon_api_handler(
+    Path(poke_name): Path<String>,
+) -> Result<Json<Value>, (StatusCode, String)> {
     let rustemon_client = rustemon::client::RustemonClient::default();
     let pokemone = rustemon::pokemon::pokemon::get_by_name(&poke_name, &rustemon_client).await;
 
     match pokemone {
-        Ok(p) => Ok(Json(json!(p.moves))),
+        Ok(p) => Ok(Json(json!(p))),
         Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, format!("Error: {}", e))),
     }
 }
 
-// async fn tcg_api_handler(
-//     Path(api_key): Path<String>,
-//     Path(card_id): Path<String>,
-// ) -> Result<Json<Value>, (StatusCode, String)> {
-//     let client = Client::with_api_key(&api_key);
-//     let carde = client
-//         .SearchCardsRequest::new("name:{}", &card_id)
-//         .await;
-//
-//     match carde {
-//         // Card
-//         Ok(c) => Ok(Json(json!(c))),
-//         // Will be a 'ClientError' enum
-//         Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, format!("Error: {}", e))),
-//     }
-// }
+async fn tcg_api_handler(
+    Path(poke_card_name): Path<String>,
+) -> Result<Json<Vec<CardBriefResponse>>, (StatusCode, String)> {
+    let result = tokio::task::spawn_blocking(move || {
+        let tcgdex = Tcgdex::new();
+        let filter_str = format!("name={}", &poke_card_name);
+        let filter = Query::new().with_filtering(vec![filter_str.as_str()]);
+        tcgdex.cards().fetch::<Vec<CardBrief>>(Some(&filter))
+    })
+    .await
+    .map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Join error: {}", e),
+        )
+    })?;
+
+    match result {
+        Ok(c) => Ok(Json(c.into_iter().map(CardBriefResponse::from).collect())),
+        Err(e) => Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Fetch error: {}", e),
+        )),
+    }
+}
